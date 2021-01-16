8ee7d2f9e457969605685707586bd5f97113033b
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2021-01-16 11:54:53 +0300

    Set up game module routing

--- src/app/game/game-routing.module.ts
+++ src/app/game/game-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './game.component';
+import { LevelsComponent } from './levels/levels.component';
+import { PlayComponent } from './play/play.component';
+import { StatsComponent } from './stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: GameComponent,
+    children: [
+      { path: 'levels', component: LevelsComponent },
+      { path: 'play', component: PlayComponent },
+      { path: 'stats', component: StatsComponent },
+      { path: '', redirectTo: 'levels', pathMatch: 'full' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -4,9 +4,11 @@ import { SharedModule } from '../shared/shared.module';
 import { GameRoutingModule } from './game-routing.module';
 import { GameComponent } from './game.component';
 import { LevelsComponent } from './levels/levels.component';
+import { PlayComponent } from './play/play.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
-  declarations: [GameComponent, LevelsComponent],
+  declarations: [GameComponent, LevelsComponent, PlayComponent, StatsComponent],
   imports: [
     SharedModule,
     GameRoutingModule
