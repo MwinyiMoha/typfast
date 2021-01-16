71272af777e682cfa181a033848744ebcbf43a2b
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2021-01-16 12:10:34 +0300

    Set up routing for auth module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResetComponent } from './reset/reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'logout', component: LogoutComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'password-reset', component: ResetComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ResetComponent } from './reset/reset.component';
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [
+    AuthComponent,
+    LoginComponent,
+    LogoutComponent,
+    ProfileComponent,
+    ResetComponent
+  ],
   imports: [
     SharedModule,
     AuthRoutingModule
